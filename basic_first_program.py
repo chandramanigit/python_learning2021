63b909d89333fa90d8752ef91067c3156484488b
chandramanigit <chandramanishoponline@gmail.com>
2021-06-23 23:08:30 +0530

    first script

---
+++ basic_first_program.py
@@ -0,0 +1,18 @@
+def sentence_make(phrase):
+    capitalized = phrase.capitalize()
+    interrogative = ("how" , "what" , "when" , "why")
+    if phrase.startswith(interrogative):
+        return "{} ?".format(capitalized)
+    else:    
+        return "{}.".format(capitalized) 
+    
+result = []
+while True:
+    user_input = input("Say something: ")
+    if user_input == "\end":
+        break
+    else:
+        result.append(sentence_make(user_input))
+    
+print(" ".join(result)) #result
+
